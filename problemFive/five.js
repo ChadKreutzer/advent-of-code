7840e13d434ab9f8445e5b5e44b7a382cc933e6a
Chad Kreutzer <kanthalion@gmail.com>
2018-12-10 09:19:32 +0000

    Day 5 done

--- problemFive/five.js
+++ problemFive/five.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const removeAntiUnits = require('./removeAntiUnits');
+const removePoly = require('./removePoly');
+const polyList = [/a/,/b/,/c/,/d/,/e/,/f/,/g/,/h/,/i/,/j/,/k/,/l/,/m/,
+                  /n/,/o/,/p/,/q/,/r/,/s/,/t/,/u/,/v/,/w/,/x/,/y/,/z/];
 
 fs.readFile("./input.txt", "utf8", (err, data) => {
   if (err) throw err;
   const input = data.trim();
   
   console.log(removeAntiUnits(input));
+  const partTwo = polyList.map(p => removeAntiUnits(removePoly(input, p)))
+    .reduce((lowest, curr) => curr < lowest ? curr : lowest);
   
+  console.log(partTwo);
 });
